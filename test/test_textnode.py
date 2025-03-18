27cb316e74948fc713cc5e03be035db14576d4a9
Stefano Labianca <lstefano@proton.me>
2025-03-18 22:56:35 +0100

    First test suit

---
+++ test/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from src.textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node",
+                         TextType.ITALIC, "http://localhost")
+        self.assertNotEqual(node, node2)
+
+    def test_invalid_text_type(self):
+        self.assertRaises(AttributeError,
+                          lambda *args, **kwargs: TextNode(
+                              "This is a text node", TextType.INVALID)
+                          )
+
+    def test_link_is_not_none(self):
+        node = TextNode("This is a text node", TextType.BOLD,
+                        "https://docs.python.org/3/library/unittest.html")
+
+        self.assertIsNotNone(node.url)
+
+
+if __name__ == "__main__":
+    unittest.main()
