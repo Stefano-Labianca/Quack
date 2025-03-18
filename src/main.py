9f303f19310474a2dc108e8b54b0e236665dc429
Stefano Labianca <lstefano@proton.me>
2025-03-18 22:20:22 +0100

    Create a TextNode class to rappresent and inline text

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+from textnode import TextNode, TextType
+
+node = TextNode("This is some anchor text",
+                TextType.LINK, "https://www.boot.dev")
+
+another_node = TextNode("This is some  text",
+                        TextType.LINK, "https://www.boot.dev")
+
+print(node)
---
+++ src/textnode.py
@@ -0,0 +1,34 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    NORMAL = 1
+    BOLD = 2
+    ITALIC = 3
+    CODE = 4
+    LINK = 5
+    IMAGE = 6
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None):
+        """
+        ### TextNode
+            - `text`: Text content of the node
+            - `text_type`: The type of text this node contains, which is a  member of the `TextType` enum
+            - `url` - The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
+        """
+
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text == other.text and
+            self.text_type == other.text_type and
+            self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.name}, {self.url})"
