14c30e9275a15afebf67c649c0ddcaebaea33cc7
Stefano Labianca <lstefano@proton.me>
2025-03-27 18:12:09 +0100

    Simplified solution when creating nodes for inline markdown

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
-from scanner import split_nodes_delimiter
+from mardown_scanner.inline_scanner import split_nodes_delimiter
 from nodes.textnode import TextNode, TextType
 
-code = TextNode("`This` is text with a `code block` word. `Funny guy`", TextType.TEXT)
-code2 = TextNode("This is `text` with a `code block` word. `Funny guy`", TextType.TEXT)
-code3 = TextNode("`This is` text with a `code block` word. Funny guy.", TextType.TEXT)
-new_nodes = split_nodes_delimiter([code, code2, code3], "`", TextType.CODE)
-print(new_nodes)
+# codes = [
+#     TextNode("`This` is text with a `code block` word. `Funny guy`", TextType.TEXT),
+#     TextNode("This is `text` with a `code block` word. `Funny guy`", TextType.TEXT),
+#     TextNode("`This is` text with a `code block` word. Funny guy.", TextType.TEXT)
+# ]
+
+
+# new_nodes = split_nodes_delimiter(codes, "`", TextType.CODE)
+# print(new_nodes)
+# print("\n-------\n")
 
 italics = [
-    TextNode("This is text with a _italic block_ word", TextType.TEXT),
+    TextNode("_This is text_ with a _italic block_ word", TextType.TEXT),
     TextNode("_Just one italic block_", TextType.TEXT),
 ]
 
+# new_nodes = split_nodes_delimiter(italics, "_", TextType.ITALIC)
+# print("\n-------\n")
 
+# bolds = [
+#     TextNode("This is text with a **bold  block** word", TextType.TEXT),
+#     TextNode("**Just one bold block**", TextType.TEXT),
+#     TextNode("**Just one bold block**. Salame e pepe e **fagioli** con *gelato", TextType.TEXT),
+# ]
+
+print("Solution")
 new_nodes = split_nodes_delimiter(italics, "_", TextType.ITALIC)
-print("\n-------\n")
-
+print(new_nodes)
