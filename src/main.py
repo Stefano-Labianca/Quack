7b475b07ece77e089d408601881e46df8f5da6a4
Stefano Labianca <lstefano@proton.me>
2025-04-03 23:02:10 +0200

    first working demo with one page

--- src/main.py
+++ src/main.py
@@ -1,50 +1,86 @@
-from convert import markdown_to_html_node
+import shutil
+import os
 
-md = """
-# Hello `const foo = "bar"`
+from convert import block_node_to_html_node, extract_title, markdown_to_html_node
+from markdown_scanner.block_scanner import markdown_to_blocks 
 
-This is **bolded** paragraph
-text in a p
-tag here
+PUBLIC_FOLDER_PATH = "./public/"
+SOURCE_FOLDER = "./static/"
+TEMPLATE_PAHT = "./template.html"
 
-> Quote _with italic_ and **bold**
-> Again quote and `quote`
+def create_public_folder():
+    os.mkdir(PUBLIC_FOLDER_PATH)
 
-- One
-- Two
-- Three
 
-1. One
-2. Two
-3. Three
+def remove_public_folder():
+    if not os.path.exists(PUBLIC_FOLDER_PATH):
+        return
+    
+    shutil.rmtree(PUBLIC_FOLDER_PATH)
 
-This is another paragraph with _italic_ text and `code` here
 
-```
-function foo() {
-    return "bar"
-}
-```
-"""
+def copy_from_static_folder(start: str = SOURCE_FOLDER):
+    print(f"Current Path: '{start}'")
+    files = os.listdir(start)
+    folders = []
 
-# md = """
-# ```
-# This is text that _should_ remain
-# the **same** even with inline stuff
-# ```
-# """
+    if len(files) == 0:
+        return
 
-node = markdown_to_html_node(md)
+    for file in files:
+        src_file_path = os.path.join(start, file)
+        base_dest_path = start.replace(SOURCE_FOLDER, "")
+        dest_file_path = os.path.join(os.path.join(PUBLIC_FOLDER_PATH, base_dest_path), file)
 
-if node != None: 
-    print(node.to_html())
-else:
-    print("Invalid")
-# md = """
-# ```
-# This is text that _should_ remain
-# the **same** even with inline stuff
-# ```
-# """
+        if os.path.isfile(src_file_path):
+            shutil.copy(src_file_path, dest_file_path)
 
-# node = markdown_to_html_node(md)
+            print(f"\t{src_file_path} moved to {dest_file_path}")
+        else:
+            folders.append(file)
+
+    for folder in folders:
+        src_file_path = os.path.join(start, folder)
+        base_dest_path = start.replace(SOURCE_FOLDER, "")
+        dest_file_path = os.path.join(os.path.join(PUBLIC_FOLDER_PATH, base_dest_path), folder)
+
+        os.mkdir(dest_file_path)
+        print()
+        copy_from_static_folder(src_file_path)
+
+
+def generate_page(from_path: str, template_path: str, dest_path: str):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    md_file = open(from_path)
+    md_content = md_file.read()
+    md_file.close()
+
+    template_file = open(template_path)
+    template_content = template_file.read()
+    template_file.close()
+
+    root = markdown_to_html_node(md_content)
+    generated_html = root.to_html()
+    page_title = extract_title(md_content)
+
+    page = template_content.replace("{{ Title }}", page_title).replace("{{ Content }}", generated_html)
+    # base_dir = os.path.dirname(dest_path)
+
+    new_html_page_file = open(dest_path, "x")
+    new_html_page_file.write(page)
+    new_html_page_file.close()
+
+
+
+def main():
+    remove_public_folder()
+    create_public_folder()
+
+    if not os.path.exists(PUBLIC_FOLDER_PATH):
+        raise FileNotFoundError(f"{PUBLIC_FOLDER_PATH} not found")
+    
+    copy_from_static_folder()
+    print("\n ------ Start Generation ------")
+    generate_page("./content/index.md", TEMPLATE_PAHT, f"{PUBLIC_FOLDER_PATH}index.html")
+
+main()
